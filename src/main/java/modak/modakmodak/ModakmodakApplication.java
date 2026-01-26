bc00c0e9bc94e7de742321acf653537a474b3c6b
이시은 <selee6047@gmail.com>
2026-01-26 13:11:12 +0900

    Spring Boot 3.5.10 버전으로 프로젝트 재시작

---
+++ src/main/java/modak/modakmodak/ModakmodakApplication.java
@@ -0,0 +1,13 @@
+package modak.modakmodak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModakmodakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModakmodakApplication.class, args);
+	}
+
+}
---
+++ src/test/java/modak/modakmodak/ModakmodakApplicationTests.java
@@ -0,0 +1,13 @@
+package modak.modakmodak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModakmodakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
