7ff8464cc079fee13983cd2156155684b88ffb84
이시은 <selee6047@gmail.com>
2026-02-14 01:53:36 +0900

    모임 상세 정보 수정 관련 코드 수정

--- src/main/java/modak/modakmodak/dto/MeetingUpdateDetailRequest.java
+++ src/main/java/modak/modakmodak/dto/MeetingUpdateDetailRequest.java
@@ -1,7 +1,18 @@
 package modak.modakmodak.dto;
 
+import modak.modakmodak.entity.MeetingAtmosphere;
+import modak.modakmodak.entity.MeetingCategory;
+
 public record MeetingUpdateDetailRequest(
+        String title,
+        String date,
         String area,
         String locationDetail,
-        String hostAnnouncement
+        String description,
+        String imageUrl,
+        String hostAnnouncement,
+        MeetingAtmosphere atmosphere,
+        MeetingCategory category,
+        String categoryEtc,
+        int maxParticipants
 ) {}
