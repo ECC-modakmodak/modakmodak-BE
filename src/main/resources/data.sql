3a8ff1e78967558fbbeb5e1f379d5894bfd6bf13
이시은 <selee6047@gmail.com>
2026-02-14 23:08:01 +0900

    더미데이터 수정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,12 +1,12 @@
 -- 1. 유저 생성
 INSERT INTO users (id, username, password, email, nickname, profile_image, preferred_type, preferred_method, activity_area, target_message, attendance_rate)
 VALUES (1, 'try2', 'pw123456', 'try2@example.com', '지인',
-        'https://modak-bucket.s3.ap-northeast-2.amazonaws.com/profile_default.png',
+        'profile_default.png',
         'CHATTY', '대면', '서울시 서대문구', '웹 개발 정복하기!', 0);
 
 INSERT INTO users (id, username, password, email, nickname, profile_image, preferred_type, preferred_method, activity_area, target_message, attendance_rate)
 VALUES (2, 'potato', 'pw123456', 'potato@example.com', '감자',
-        'https://modak-bucket.s3.ap-northeast-2.amazonaws.com/profile_default.png',
+        'profile_default.png',
         'CHATTY', '대면', '서울시 서대문구', '웹 개발 정복하기!', 0);
 
 -- 2. 모임 생성 (host_announcement 컬럼을 추가해야 합니다!)
@@ -14,7 +14,7 @@ INSERT INTO meeting (id, user_id, title, category, atmosphere, max_participants,
 VALUES (1, 1, '모닥모닥 첫 모임', 'CAFE', 'CHATTY', 5, '서울시 서대문구', '이화여대 ECC 1번 테이블',
         '방장이 등록한 공지사항이 이곳에 표시됩니다.',
         '2026-02-20T19:00:00', 'AVAILABLE', '함께 열심히 코딩해요!',
-        'https://modak-bucket.s3.ap-northeast-2.amazonaws.com/pod_1.png');
+        'pod_1.png');
 
 -- 3. 참여자 연결
 INSERT INTO participant (meeting_id, user_id, status)
