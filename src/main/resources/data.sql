286a2ab1a317e335c8d223df478f367fa91a9dc3
이시은 <selee6047@gmail.com>
2026-02-15 16:11:42 +0900

    더미데이터 수정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -9,6 +9,23 @@ VALUES (2, 'potato', 'pw123456', 'potato@example.com', '감자',
         'profile_default.png',
         'CHATTY', '대면', '서울시 서대문구', '웹 개발 정복하기!', 0);
 
+INSERT INTO users (id, username, password, email, nickname, profile_image, preferred_type, preferred_method, activity_area, target_message, attendance_rate)
+VALUES (3, 'modaki', 'modak123456', 'modaki@example.com', '모다기',
+        'profile_default.png',
+        'CHATTY', '비대면', '서울시 용산구', '열심히 공부하기!', 0);
+
+
+INSERT INTO users (id, username, password, email, nickname, profile_image, preferred_type, preferred_method, activity_area, target_message, attendance_rate)
+VALUES (5, 'study', 'study123456', 'study@example.com', '공부좀하자',
+        'profile_default.png',
+        'QUIET', '대면', '서울시 구로구', '공부 해보자!', 0);
+
+INSERT INTO users (id, username, password, email, nickname, profile_image, preferred_type, preferred_method, activity_area, target_message, attendance_rate)
+VALUES (6, 'sleep', 'sl123456', 'sleep@example.com', '자고싶어요',
+        'profile_default.png',
+        'QUIET', '대면', '서울시 서대문구', '공부 마스터!', 0);
+
+
 -- 2. 모임 생성 (host_announcement 컬럼을 추가해야 합니다!)
 INSERT INTO meeting (id, user_id, title, category, atmosphere, max_participants, area, location_detail, host_announcement, date, status, description, image_url)
 VALUES (1, 1, '모닥모닥 첫 모임', 'CAFE', 'CHATTY', 5, '서울시 서대문구', '이화여대 ECC 1번 테이블',
