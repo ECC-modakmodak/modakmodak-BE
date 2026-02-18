2709ec1a26a1c362977b6f9265d3b86c6e456f0a
이시은 <selee6047@gmail.com>
2026-02-19 01:32:46 +0900

    enum 변경

--- src/main/java/modak/modakmodak/entity/MeetingMethod.java
+++ src/main/java/modak/modakmodak/entity/MeetingMethod.java
@@ -4,6 +4,22 @@ import io.swagger.v3.oas.annotations.media.Schema;
 
 @Schema(description = "선호 모임 방식")
 public enum MeetingMethod {
-    //대면, 비대면
-    ONLINE, OFFLINE
+    ONLINE("비대면"),
+    OFFLINE("대면");
+
+    private final String value;
+    MeetingMethod(String value) { this.value = value; }
+
+    // JSON 데이터를 자바 객체로 바꿀 때 한글을 인식하게 해주는 마법의 어노테이션
+    @com.fasterxml.jackson.annotation.JsonCreator
+    public static MeetingMethod from(String value) {
+        for (MeetingMethod method : MeetingMethod.values()) {
+            if (method.value.equals(value)) return method;
+        }
+        return null;
+    }
+
+    // DB나 API 응답 시 한글로 보여주고 싶을 때
+    @com.fasterxml.jackson.annotation.JsonValue
+    public String getValue() { return value; }
 }
